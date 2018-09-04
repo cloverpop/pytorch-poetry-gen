f9e936909baf4581380186725cdd655dcad8d6f2
Zhifei Yang <zhifei.yang@arm.com>
2018-09-04 16:44:04 +0800

    Test/Debug/Verify dataHandler.py
    Signed-off-by: Zhifei Yang <zhifei.yang@arm.com>

--- dataHandler.py
+++ dataHandler.py
@@ -4,11 +4,15 @@ import os
 import json
 import re
 
+from sys import exit
+
 def parseRawData(author = None, constrain = None):
     rst = []
 
     def sentenceParse(para):
-        # para = "-181-村橋路不端，數里就迴湍。積壤連涇脉，高林上笋竿。早嘗甘蔗淡，生摘琵琶酸。（「琵琶」，嚴壽澄校《張祜詩集》云：疑「枇杷」之誤。）好是去塵俗，煙花長一欄。"
+        if para == "":
+            para = "-181-村橋路不端，數里就迴湍。積壤連涇脉，高林上笋竿。早嘗甘蔗淡，生摘琵琶酸。（「琵琶」，嚴壽澄校《張祜詩集》云：疑「枇杷」之誤。）好是去塵俗，煙花長一欄。"
+            #para = "-181-村橋路不端，數里就迴湍。積壤連涇脉，高林上笋竿。早嘗甘蔗淡，生摘琵琶酸。好是去塵俗，煙花長一欄。"
         result, number = re.subn("（.*）", "", para)
         result, number = re.subn("（.*）", "", para)
         result, number = re.subn("{.*}", "", result)
@@ -23,13 +27,16 @@ def parseRawData(author = None, constrain = None):
         return r
 
     def handleJson(file):
-        # print file
+        #print file
         rst = []
         data = json.loads(open(file).read())
         for poetry in data:
             pdata = ""
             if (author!=None and poetry.get("author")!=author):
                 continue
+            #
+            # To check if there is a valid "paragraphs" section
+            #
             p = poetry.get("paragraphs")
             flag = False
             for s in p:
@@ -42,22 +49,41 @@ def parseRawData(author = None, constrain = None):
                         break
             if flag:
                 continue
+
             for sentence in poetry.get("paragraphs"):
                 pdata += sentence
+
+            #
+            # pdata is the Chinese
+            #    斷崖如削瓜，嵐光破崖綠。天河從中來，白雲漲川谷。玉案赤文字，世眼不可讀。攝身凌青霄，松風拂我足。
+            #
+            # rst is a List data structure. Its element is object.
+            #
             pdata = sentenceParse(pdata)
             if pdata!="":
                 rst.append(pdata)
         return rst
-    # print sentenceParse("")
+
+    #
+    # Self test the sentenceParse() function
+    #
+    #print sentenceParse("")
+
+    #
+    # data is a List data strcuture
+    #
     data = []
-    src = './chinese-poetry/json/'
+    src = os.getcwd() + os.sep+ './chinese-poetry/json/'
     for filename in os.listdir(src):
+        #
+        #print("Filename="+ filename)
+        #
         if filename.startswith("poet.tang"):
             data.extend(handleJson(src+filename))
     return data
 
 
 
-# if __name__=='__main__':
+#if __name__=='__main__':
 #     print parseRawData.sentenceParse("熱暖將來賓鐵文，暫時不動聚白雲。撥卻白雲見青天，掇頭裏許便乘仙。（見影宋蜀刻本《李太白文集》卷二十三。）（以上繆氏本《太白集》）-362-。")
-#
+
