d310142f2e14e5f2334a94bbf4555a5980b11520
Zhifei Yang <zhifei.yang@arm.com>
2018-09-07 17:54:37 +0800

    Update sample.py
    Signed-off-by: Zhifei Yang <zhifei.yang@arm.com>

--- sample.py
+++ sample.py
@@ -1,12 +1,25 @@
 # coding:utf-8
+
+from __future__ import print_function
+
 import torch
 import cPickle as p
 from utils import *
+
+
+
+if torch.cuda.is_available():
+    CUDA_GPU = 1
+else:
+    CUDA_GPU = 0
+    print( "No GPU CUDA support")
 
 model = torch.load('poetry-gen.pt')
 max_length = 100
 rFile = file('wordDic', 'r')
 word_to_ix = p.load(rFile)
+
+print("len(word__to_ix)=[%d]"%(len(word_to_ix)))
 
 def invert_dict(d):
     return dict((v, k) for k, v in d.iteritems())
@@ -23,7 +36,10 @@ def sample(startWord='<START>'):
     if (startWord != "<START>"):
         output_name = startWord
     for i in range(max_length):
-        output, hidden = model(input.cuda(), hidden)
+        if CUDA_GPU:
+            output, hidden = model(input.cuda(), hidden)
+        else:
+            output, hidden = model(input, hidden)
         topv, topi = output.data.topk(1)
         topi = topi[0][0]
         w = ix_to_word[topi]
@@ -36,11 +52,11 @@ def sample(startWord='<START>'):
 
 
 
-print sample("春".decode('utf-8'))
-print sample("花".decode('utf-8'))
-print sample("秋".decode('utf-8'))
-print sample("月".decode('utf-8'))
-print sample("夜".decode('utf-8'))
-print sample("山".decode('utf-8'))
-print sample("水".decode('utf-8'))
-print sample("葉".decode('utf-8'))
+print( sample("春".decode('utf-8')))
+print( sample("花".decode('utf-8')))
+print( sample("秋".decode('utf-8')))
+print( sample("月".decode('utf-8')))
+print( sample("夜".decode('utf-8')))
+print( sample("山".decode('utf-8')))
+print( sample("水".decode('utf-8')))
+print( sample("葉".decode('utf-8')))
