0c9f02980e3d68c038b50109861a5ace74375f58
Zhifei Yang <zhifei.yang@arm.com>
2018-12-14 13:55:58 +0800

    Update the chinese_translate.py, which can convert the TraditionalChinese poems to SimplifiedChinese poems.
    Signed-off-by: Zhifei Yang <zhifei.yang@arm.com>

--- chinese_translate.py
+++ chinese_translate.py
@@ -6,35 +6,81 @@ import re
 import json
 import codecs
 
-from langconv import *
-
+from sc_2_tc import zh2Hant
+from tc_2_sc import zh2Hans
+    
 #---------------------------------------------------
 # Global constant
 #
-POEM_PATH = 'chinese-poetry' + os.sep + "json" 
+global json_list
+
+# original json root folder
+#JSON_ROOT = os.getcwd()
+#JSON_ROOT= "C:\zyang\workspace\pytorch-poetry-gen\pytorch-poetry-gen\chinese-poetry\json"
+JSON_ROOT = os.getcwd() 
+
+# new json generated root folder
+JSON_GEN_ROOT = os.getcwd() + os.sep + 'gen'
+
 INPUT_FILE = "poet.song.1000.json"
 
-#
-# This API uses the langconv library, which cannot translate TC->SC very well.
-#
-def Traditional2Simplified(sentence):
-    '''
-    将sentence中的繁体字转为简体字
-    :param sentence: 待转换的句子
-    :return: 将句子中繁体字转换为简体字之后的句子
-    '''
-    sentence = Converter('zh-hans').convert(sentence)
-    return sentence
+# convert TC to SC
+def tc_2_sc(sentence):
+    assert(sentence != None)
 
-
-def read_file_part_one():
-    remains = ""
+    sc_sentence = ''
     
-    file=os.getcwd() + os.sep + POEM_PATH + os.sep + INPUT_FILE
+    for i in range(len(sentence)):
+        tc = sentence[i]
+        try:
+            sc = zh2Hans[tc]
+        except KeyError:
+            # loop again to see if sc is a sentence
+            tmp_s = ''
+            for j in range(len(tc)):
+                tmp_tc = tc[j]
+                try:
+                    tmp_sc = zh2Hans[ tmp_tc ]
+                except KeyError:
+                    tmp_sc = tmp_tc
+                tmp_s += tmp_sc
+            
+            sc = tmp_s            
+        sc_sentence += sc
+
+    return sc_sentence
+
+# convert SC to TC
+def sc_2_tc(sentence):
+    assert(sentence != None)
+
+    tc_sentence = ''
+    
+    for i in range(len(sentence)):
+        sc = sentence[i]
+        try:
+            tc = zh2Hant[sc]
+        except KeyError:
+            tc = sc
+        tc_sentence += tc
+
+    return tc_sentence
+
+
+def parse_and_generate_json(infile, outfile):
+    assert( infile != None  )
+    assert( outfile != None )
+    
+    fin = None
+    fout = None
     
     try:
-        with codecs.open(file, 'r', 'utf-8') as fin:
+        fout = codecs.open(outfile, "wb", "utf-8")
+
+        
+        with codecs.open(infile, 'rb', 'utf-8') as fin:
             data = json.load(fin)
+            data_out = {}
 
             #
             # can print all Chinese poems correctly
@@ -45,43 +91,137 @@ def read_file_part_one():
             print("poem count=%d"%(poem_count))
 
             for i in range(poem_count):
-                author = data[i]["author"]
+                #
+                # Processing 'author'
+                #
+                try:
+                    tc_author = data[i]["author"]
+
+                    #
+                    # This will output the utf8 format author
+                    #
+                    #print(tc_author.encode('utf8'))
+
+                    #
+                    # This will output the Unicode format author (Chinese)
+                    #
+                    # print(tc_author)
+
+                    sc_author = tc_2_sc(tc_author)
+                    #print(sc_author)
+
+                    #print(tc_author + "\t==>\t" + sc_author + "\t==>\t" + tc_author)
+                    data_out['author'] = sc_author
+                    
+                except KeyError:
+                    # this is not a poem
+                    pass                
+
+                #
+                # Processing 'title'
+                #
+                try:
+                    tc_title = data[i]["title"] 
+                    sc_title = tc_2_sc(tc_title)
+
+                    #tc2_title = sc_2_tc(sc_title)
+                    #print(tc_title + "\t==>\t" + sc_title + "\t==>\t" + tc2_title)
+
+                    data_out['title'] = sc_title
+                except KeyError:
+                    pass
+
+                #
+                # Processing 'strains'
+                #
+                try:
+                    tc_strains = data[i]['strains']
+                    sc_strains = tc_2_sc(tc_strains)
+                    data_out['strains'] = sc_strains
                 
-                #
-                # This will output the utf8 format author
-                #
-                #print(author.encode('utf8'))
+                except KeyError:
+                    pass
 
                 #
-                # This will output the Unicode format author (Chinese)
+                # Processing 'paragraphs'
                 #
-                # print(author)
+                try:
+                    tc_poem = data[i]["paragraphs"]
 
-                #sc_author = Traditional2Simplified(author)
-                #print(sc_author)
+                    # paragraph is special
+                    tmp_poem = ''
+                    for i in range(len(tc_poem)):
+                        tc = tc_poem[i]
+                        try:
+                            #sc = zh2Hans[tc]
+                            sc = tc_2_sc(tc)
+                        except KeyError:
+                            sc = tc
+                        #print( tc + "\t=>\t" + sc)
+                        tmp_poem += sc
+                    #print(tmp_poem)
+                    data_out["paragraphs"] = tmp_poem
+                except KeyError:
+                    pass
 
-                tc_title = data[i]["title"]
-                sc_title = Traditional2Simplified(tc_title)
-                print("\t\t\t" + sc_title)
+                #
+                # Processing 'name'
+                #
+                try:
+                    tc_name = data[i]['name']
+                    sc_name = tc_2_sc(tc_name)
+                    data_out['name'] = sc_name
+                except KeyError:
+                    pass
 
-                tc_poem = data[i]["paragraphs"]
-                sc_poem = Traditional2Simplified(tc_poem)
-                print(sc_poem)
+                #
+                # Processing 'desc'
+                #
+                try:
+                    tc_desc = data[i]['desc']
+                    sc_name = tc_2_sc(tc_desc)
+                    data_out['desc'] = sc_name
+                except KeyError:
+                    pass
                 
-
-        
-        
-
-
+                #print(data[i])
+                json.dump(data_out, fout, ensure_ascii=False, sort_keys=True, indent=4, separators=(',', ':'))
+                #break
+                
         fin.close()
+        fout.close()
         
     except IOError:
-        print("Failed to open %s"%(file))
-            
-
-    
+        print("Failed to open %s"%(infile))
+        
     pass
 
+def parse_all_json(root):
+    global json_list
+    
+    assert(root!=None)
+    valid_json = 0
+    
+    for root, dirs, files in os.walk(root):
+        for filename in files:
+            if filename.rfind('.json') != -1:
+                path = root + os.sep + filename
+                #print(path)
+                json_list.append(path)
+
+                infile = path
+                if infile.rfind(os.sep+ 'gen' + os.sep) != -1:
+                    continue
+
+                valid_json += 1
+                
+                outfile = JSON_GEN_ROOT + os.sep + filename
+                print(infile)
+                print(outfile)
+                parse_and_generate_json(infile, outfile)
+
+    
+    print("Total json file count=%d"%(valid_json))
 
 def test():
     m = {'a' : '您好'}
@@ -97,8 +237,9 @@ def test():
 
 
 if __name__ == "__main__":
-    test()
+    global json_list
+    json_list = []
     
-    read_file_part_one()
+    parse_all_json(JSON_ROOT)
 
     pass
